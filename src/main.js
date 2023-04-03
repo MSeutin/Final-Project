9c9f04e5e295847c29653d6057ac99088af75986
Michael Seutin <frenchmike@gmail.com>
2023-04-02 18:35:03 -0700

    added model

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 // LOCAL IMPORTS
 
-// CONSTANTS
+// VARIABLES
+let soldier;
+let hemiLight;
+
 
 ///////////////////////////
 //         SCENE
 ///////////////////////////
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xcccccc );
 
 ///////////////////////////
 //        CAMERA
@@ -26,6 +31,7 @@ camera.position.y = 2;
 //       RENDERER
 ///////////////////////////
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 // add renderer to HTML document. This is a <canvas> element 
 document.body.appendChild( renderer.domElement );
@@ -33,10 +39,20 @@ document.body.appendChild( renderer.domElement );
 ///////////////////////////
 //      CUBE
 ///////////////////////////
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({ color: 'crimson' });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const geometry = new THREE.BoxGeometry(1,1,1);
+// const material = new THREE.MeshBasicMaterial({ color: 'crimson' });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// MODEL WITH ANIMATIONS
+const loader = new GLTFLoader();
+loader.load('models/avenger.glb', function (gltf) {
+    soldier = gltf.scene;  // sword 3D object is loaded
+    soldier.scale.set(1, 1, 1);
+    soldier.rotation.y = Math.PI; //-90 degrees around the xaxis
+    soldier.castShadow = true;
+    scene.add(soldier);
+});
 
 
 ///////////////////////////
@@ -48,22 +64,57 @@ grassTex.wrapS = THREE.RepeatWrapping;
 grassTex.wrapT = THREE.RepeatWrapping; 
 grassTex.repeat.x = 256; 
 grassTex.repeat.y = 256; 
-let groundTexture = new THREE.MeshBasicMaterial({map:grassTex}); 
+let groundTexture = new THREE.MeshStandardMaterial({map:grassTex}); 
 
 const groundGeometry = new THREE.PlaneGeometry(400,400);
-const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const groundMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
 const ground = new THREE.Mesh(groundGeometry, groundTexture);
 ground.position.y = -10;
 ground.rotation.x = -Math.PI/2; //-90 degrees around the xaxis
 ground.doubleSided = true;
+ground.receiveShadow = true;
 scene.add(ground);
+
+// movement
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    let keyCode = event.which;
+    console.log(keyCode);
+    if (keyCode == 38) {
+        // forward
+        soldier.position.z -= 1;
+    } else if (keyCode == 40) {
+        // backward
+        soldier.position.z += 1;
+    } else if (keyCode == 37) {
+        // left
+        soldier.position.x -= 1;
+    } else if (keyCode == 39) {
+        // right
+        soldier.position.x += 1;
+    } else if (keyCode == 32) {
+        // spacebar to recenter
+        soldier.position.x = 0.0;
+        soldier.position.y = 0.0;
+    }
+}
+
+
 
 ///////////////////////////
 //      LIGHT
 ///////////////////////////
 const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1);
+light.position.set(1, 15, 10);
+light.castShadow = true;
 scene.add(light);
+const ambientLight = new THREE.AmbientLight( 0xcccccc ); // soft white light
+scene.add( ambientLight );
+
+
+hemiLight = new THREE.HemisphereLight( "orange", 0x080820, 4);
+scene.add( hemiLight );
+
 
 ///////////////////////////
 //      CONTROLS
