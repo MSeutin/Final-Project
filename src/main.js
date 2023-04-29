f4c970532b402e6f499de1313583feb20a957325
Anudari Gereltod <anuu1799@gmail.com>
2023-04-28 22:20:28 -0700

    Added car rotation and constant moving forward

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import TWEEN from '@tweenjs/tween.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 // LOCAL IMPORTS
@@ -40,6 +41,7 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // or any other type of shadow map
 renderer.shadowMap.autoUpdate = true;
 renderer.shadowMap.needsUpdate = true;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 // add renderer to HTML document. This is a <canvas> element 
 document.body.appendChild( renderer.domElement );
@@ -50,6 +52,7 @@ loader.load('models/car.glb', function (gltf) {
     player = gltf.scene;  // sword 3D object is loaded
     player.scale.set(2, 2, 2);
     player.rotation.y = Math.PI; //-90 degrees around the x-axis
+
     player.traverse(function (node){  // traverse through the model elements
         if(node.isMesh){  // check if it is a mesh
             node.castShadow = true;
@@ -77,10 +80,12 @@ ground.doubleSided = true;
 ground.receiveShadow = true;
 scene.add(ground);
 
-// Set Original Positions
-// playerTarget.Position.copy(player.position);
-// playerTarget.Rotation.copy(player.quaternion);
+function movePlayerForward(playerPosition) {
+    playerPosition.z -= 1; // move the player forward by decreasing the z-coordinate
+    player.position.copy(playerPosition); // update the player's position
+}
 
+let moveForward = false;
 
 // movement
 document.addEventListener("keydown", onDocumentKeyDown, false);
@@ -91,38 +96,36 @@ function onDocumentKeyDown(event) {
     if (keyCode == 38 || keyCode == 75) {
         // forward
         player.position.z -= 1;
-        playerTarget.Position.copy(player.position);
-        playerTarget.Rotation.copy(player.quaternion);    
-        thirdPersonCamera.Update();
+        
+        // update the velocity instead of just position
+    } else if(keyCode == 13){  
+        // player needs to hit enter to move forward and start the game
+        moveForward = true;
+
     } else if (keyCode == 40 || keyCode == 74) {
         // backward
-        player.position.z += 1;
-        playerTarget.Position.copy(player.position);
-        playerTarget.Rotation.copy(player.quaternion);    
-        thirdPersonCamera.Update();
+        player.position.z += 0.2;   
     } else if (keyCode == 37 || keyCode == 72) {
         // left
-        player.position.x -= 1;
-        playerTarget.Position.copy(player.position);
-        playerTarget.Rotation.copy(player.quaternion);    
-        thirdPersonCamera.Update();
+        // player.position.x -= 1; 
+        player.rotation.y += Math.PI/50;  
     } else if (keyCode == 39 || keyCode == 76) {
         // right
-        player.position.x += 1;
-        playerTarget.Position.copy(player.position);
-        playerTarget.Rotation.copy(player.quaternion);    
-        thirdPersonCamera.Update();
+        player.rotation.y -= Math.PI/50; 
+        player.position.z -= 0.1; 
+
     } else if (keyCode == 32) {
         // spacebar to recenter
         location.reload();
     }
+    playerTarget.Position.copy(player.position);
+    playerTarget.Rotation.copy(player.quaternion); 
 }
 
 ///////////////////////////
 //      LIGHT
 ///////////////////////////
 const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 17, 6);
 light.castShadow = true;
 scene.add(light);
 
@@ -142,10 +145,14 @@ controls.dampingFactor = 0.25;
 ///////////////////////////
 function animate(){
     requestAnimationFrame(animate);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    thirdPersonCamera.Update();
+    if (moveForward) {
+        player.position.z -= 0.1; // Move player forward
+    }
+    
+    // player.position.z -= 0.1;
+    playerTarget.Position.copy(player.position);
     playerTarget.Rotation.copy(player.quaternion); 
+    thirdPersonCamera.Update();
     renderer.render(scene, camera);
 }
 
